bbdaff408c53ec49fc0d7afc5fa4913654eac65f
Daniel Cavalli <leaodrj@gmail.com>
2025-03-23 20:51:33 +0000

    organizando as CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,14 +3,14 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
-# São paulo corresponde a letra A
-# Minas Gerais corresponde a letra B
-# Rio Grande do sul corresponde a letra C
-# Bahia corresponde a letra D
-# Rio de janeiro corresponde a letra E
-# Maranhão corresponde a letra F
-# Piauí corresponde a letra G
-# Amazonas corresponde a letra H
+  //São paulo corresponde a letra A
+  //Minas Gerais corresponde a letra B
+  //Rio Grande do sul corresponde a letra C
+  //Bahia corresponde a letra D
+  //Rio de janeiro corresponde a letra E
+  //Maranhão corresponde a letra F
+  //Piauí corresponde a letra G
+  //Amazonas corresponde a letra H
 
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
